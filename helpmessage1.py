3b920bdfc3e06fa3580cb423455f9c5080624b7a
steveLim0928 <48382745+steveLim0928@users.noreply.github.com>
2019-03-12 19:49:22 +0800

    Delete helpmessage1.py

--- helpmessage1.py
+++
@@ -1,6 +0,0 @@
-from Tkinter import*
-import tkMessageBox
-tkMessageBox.showinfo("Help",'''To learn about Single Slit Diffraction, Press Single Slit\n
-To learn about Double Slit Diffraction, Press Double Slit\n
-Press Quiz to Proceed to Quiz Session\n
-Press the Arrow to Go Back''')
