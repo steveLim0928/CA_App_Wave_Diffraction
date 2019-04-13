2faf6454519eed338884de91f7777c3dd6efd64b
steveLim0928 <48382745+steveLim0928@users.noreply.github.com>
2019-04-13 11:11:37 +0800

    Add files via upload

---
+++ Increment_4_APP_0.1/DS_Graph.py
@@ -0,0 +1,58 @@
+import Tkinter as tk
+
+
+
+
+from numpy import arange, cos, pi
+
+
+
+
+
+def DS_Graph1 (canvas, a, z, colour):
+
+    fr = 5 #frequency
+
+    x5 = arange(98,169)#70
+    def smallamplitude1(x):
+        Fs = 350
+        y = -0.8*cos(2*pi*fr*(x-(98))/Fs)
+        a.plot(y+0.8,x5,color=colour)
+        a.set_title('Double Slit Diffraction')
+        a.set_ylim(95,498)
+        a.set_xlim(-0.04,8)
+        a.axis('off')
+    smallamplitude1(x5)
+
+    x6 = arange(168,254)#85
+    def smallamplitude1(x):
+        Fs = 425
+        y = -2.5*(cos(2*pi*fr*(x-(168))/Fs))
+        a.plot(y+2.5,x6,color=colour)
+    smallamplitude1(x6)
+
+    x7 = arange(253,344)#90
+    def smallamplitude1(x):
+        Fs= 450
+        y = -4*(cos(2*pi*fr*(x-(253))/Fs))
+        a.plot(y+4,x7,color=colour)
+    smallamplitude1(x7)
+
+    x8 = arange(343,429)#85
+    def smallamplitude1(x):
+        Fs = 425
+        y = -2.5*(cos(2*pi*fr*(x-(343))/Fs))
+        a.plot(y+2.5,x8,color=colour)
+    smallamplitude1(x8)
+
+    x9 = arange(428,499)#70
+    def smallamplitude1(x):
+        Fs = 350
+        y = -1*(cos(2*pi*fr*(x-(428))/Fs))
+        a.plot(y+1,x9,color=colour)
+    smallamplitude1(x9)    
+
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+
+
